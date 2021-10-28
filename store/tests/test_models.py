14daa673e478771d386656e95e6d5bd32761be08
Imerzel <zach.imerzel@gmail.com>
2021-10-28 18:05:34 -0500

    configured urls, adding templates

--- store/tests/test_models.py
+++ store/tests/test_models.py
@@ -11,3 +11,7 @@ class TestCategoriesModel(TestCase):
         def test_category_model_entry(self):
             data =self.data1
             self.assertTrue(isinstance(data, Category))
+
+        def test_category_model_return(self):
+            data =self.data1
+            self.assertEqual(str(data), 'shirt')
